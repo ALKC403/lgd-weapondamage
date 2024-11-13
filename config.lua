92fe031a455d247745535f588b34e74e7180c728
Alaa (LGD) <46872410+ALKC403@users.noreply.github.com>
2024-11-13 16:58:40 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,15 @@
--------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-- Script made by LGD
+-----------------------------------------------------------------------------
 -- Weapons list: https://www.vespura.com/fivem/weapons/stats/
--------------------------------------------------------------------
+-----------------------------------------------------------------------------
 -- weapon_name: weapon's name 
--------------------------------------------------------------------
+-----------------------------------------------------------------------------
 -- damage_multiplier: default is 1.0
 -- Example: 0.4 is 40% the original damage
--------------------------------------------------------------------
--- To enable or disable headshots go to: client.lua line 7
--------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-- Disabling headshots will make them count as torso damage not a 1 kill shot
+-----------------------------------------------------------------------------
 
 
 Config = {}
@@ -16,3 +18,5 @@ Config.Weapons = {
     { weapon_name = 'weapon_pistol', damage_multiplier = 0.50 },
     { weapon_name = 'weapon_ceramicpistol', damage_multiplier = 0.10 },
 }
+
+Config.Headshots = false -- true: headshots enabled / false: headshots disabled
